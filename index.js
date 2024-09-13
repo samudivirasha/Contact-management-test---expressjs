3824d7d9668b63f13c0e1f67335d1f06bd99f949
H K S V Amarakeerthi <samudiperera2000@gmail.com>
2024-09-13 16:47:16 +0530

    mongodb connection

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
 const express = require('express');
+const mongoose = require("mongoose");
+
 const app = express();
 const port = 3000;
-
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+const mongoURI = "mongodb://localhost:27017/";
+mongoose.connect(mongoURI,{useNewUrlParser: true, useUnifiedTopology: true});
+
+
+const db = mongoose.connection;
+db.on ('error',console.error.bind(console, 'Mongodb connection error:'));
+db.once('open', () => {
+console.log('connected to mongodb');
+});
+
+const contactsSchema = new mongoose.Schema({
+   name : String,
+   contact : String
+});
+
+const Contact = mongoose.model('contact', contactsSchema);
 
 //let x = 5;
 
-let mymap = new Map();
-
+//let mymap = new Map();
+app.post('/adddata', function(req, res) {
+  var reqname = req.body.name;
+  var reqcontact = req.body.contact;
+  
+  
+  const cont = new Contact({
+    name:reqname,
+    contact:reqcontact
+  })
+  
+  cont
+  .save()
+  .then(() => res.send(req.body))
+  .error((err) => res.send(err));
+  });
 
 
 
 // Sample route
 app.get('/showdata', (req, res) => {
-  const mapObj = Object.fromEntries(mymap);
+  //const mapObj = Object.fromEntries(mymap);
   res.json(mapObj);
 });
 
@@ -24,14 +55,7 @@ app.get('/showdata/:name', (req, res) => {
 });
 
 
-app.post('/adddata', function(req, res) {
-var name = req.body.name;
-var contact = req.body.contact;
 
-mymap.set (name,contact);
-  console.log('body is ',req.body);
-  res.send(req.body);
-});
 
 app.post('/showdatapost', function(req, res) {
   var name = req.body.name;
